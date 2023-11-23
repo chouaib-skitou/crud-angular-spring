dd057b5fabf35a0598471a8b564a4556b9e5a0a1
Chouaib <chouaib.skt@gmail.com>
2023-11-24 00:42:41 +0100

    fix

--- backend/src/main/java/com/javacrud/backend/exception/RessourceNotFoundException.java
+++ backend/src/main/java/com/javacrud/backend/exception/RessourceNotFoundException.java
@@ -1,4 +1,6 @@
 package com.javacrud.backend.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class RessourceNotFoundException extends RuntimeException{
