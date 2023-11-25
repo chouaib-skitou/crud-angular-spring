7750146fea3e169f4071557ad8eb7fa6319f9380
Chouaib <chouaib.skt@gmail.com>
2023-11-25 23:46:04 +0100

    add create-employee component

---
+++ frontend/src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrl: './create-employee.component.css'
+})
+export class CreateEmployeeComponent {
+
+}
