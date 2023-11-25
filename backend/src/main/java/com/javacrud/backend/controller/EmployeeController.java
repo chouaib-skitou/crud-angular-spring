f79c84228f7a7b75744ecc7d277ff2301d633739
Chouaib <chouaib.skt@gmail.com>
2023-11-25 03:54:33 +0100

    create reste api to get all employees

---
+++ backend/src/main/java/com/javacrud/backend/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.javacrud.backend.controller;
+
+import com.javacrud.backend.repository.EmployeeRepository;
+import com.javacrud.backend.model.Employee;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+    // get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
